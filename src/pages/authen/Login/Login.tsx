c18901a3ad0c3025cdece91c63d61d2c1af26522
khacSon0907 <creatine09072004@gmail.com>
2024-02-16 10:21:43 -0800

    on ok di

--- src/pages/authen/Login/Login.tsx
+++ src/pages/authen/Login/Login.tsx
@@ -43,7 +43,7 @@ const Login = () => {
             <div className="email">
               <div>
                 <i className="fa-solid fa-envelope iconEmail"></i>
-                <input type="text" placeholder='Email hoặc UserName' name='loginUser' />
+                <input type="text" placeholder='Email or UserName' name='loginUser' />
               </div>
             </div>
 
